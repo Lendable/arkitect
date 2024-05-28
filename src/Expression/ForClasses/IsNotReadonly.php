abc1928f46de6680a9c34f324e59e5b9af440f39
Ben Convey <ben.convey@lendable.co.uk>
2024-05-28 14:50:29 +0100

    WIP

--- src/Expression/ForClasses/IsNotReadonly.php
+++ src/Expression/ForClasses/IsNotReadonly.php
@@ -13,12 +13,12 @@ use Arkitect\Rules\Violations;
 
 class IsNotReadonly implements Expression
 {
-    public function describe(ClassDescription $theClass, string $because): Description
+    public function describe(ClassDescription $theClass, string $because = ''): Description
     {
         return new Description("{$theClass->getName()} should not be readonly", $because);
     }
 
-    public function evaluate(ClassDescription $theClass, Violations $violations, string $because): void
+    public function evaluate(ClassDescription $theClass, Violations $violations, string $because = ''): void
     {
         if (!$theClass->isReadonly()) {
             return;
--- src/Expression/ForClasses/IsReadonly.php
+++ src/Expression/ForClasses/IsReadonly.php
@@ -13,12 +13,12 @@ use Arkitect\Rules\Violations;
 
 class IsReadonly implements Expression
 {
-    public function describe(ClassDescription $theClass, string $because): Description
+    public function describe(ClassDescription $theClass, string $because = ''): Description
     {
         return new Description("{$theClass->getName()} should be readonly", $because);
     }
 
-    public function evaluate(ClassDescription $theClass, Violations $violations, string $because): void
+    public function evaluate(ClassDescription $theClass, Violations $violations, string $because = ''): void
     {
         if ($theClass->isReadonly() || $theClass->isInterface() || $theClass->isTrait() || $theClass->isEnum()) {
             return;
